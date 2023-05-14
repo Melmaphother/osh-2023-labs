2f6a65c95b5f5f1b53bd6e86817179202d46d087
Melmaphother <1559792558@qq.com>
2023-05-14 13:02:22 +0800

    update README.md

--- lab3/build/src/server.c.d
+++
@@ -1,3 +0,0 @@
-build/./src/server.c.o: src/server.c src/server.h src/thread.h
-src/server.h:
-src/thread.h:
--- lab3/build_epoll/src_epoll/server_epoll.c.d
+++
@@ -1,3 +0,0 @@
-build_epoll/./src_epoll/server_epoll.c.o: src_epoll/server_epoll.c \
- src_epoll/server_epoll.h
-src_epoll/server_epoll.h:
